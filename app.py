427093daaf7ded95fca3c4db45291473ced357b0
ice <ice45571@163.com>
2018-11-21 20:50:46 +0800

    feat: 引入watch-dog

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
-import json
 from account import Account
+import json
+
 
 def after_request(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
@@ -8,21 +9,55 @@ def after_request(response):
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
     return response
 
+
 app = Flask(__name__)
 app.after_request(after_request)
 accountManager = Account()
+
 
 @app.route('/welcome')
 @app.route('/')
 def index():
     return "<h> hello </h>"
 
+
+@app.route('/get')
+def testGet():
+    """
+    ex: http://localhost:5000/get?name=ice
+    """
+    name = request.args.get('name')
+    if name:
+        return 'name = {}'.format(request.args.get('name'))
+    else:
+        return json.dumps({
+            'msg': 'name为空',
+            'code': '404'
+        }), 404, ''
+
+
 @app.route('/account/register/<string:accountnumber>/<string:password>')
 def register(accountnumber, password):
     return accountManager.register(accountnumber, password)
+
 
 @app.route('/account/login/<string:accountnumber>/<string:password>')
 def login(accountnumber, password):
     return accountManager.login(accountnumber, password)
 
-app.run()
+
+if __name__ == '__main__':
+    app.run()
+
+#
+#
+# custom cli
+import click
+
+
+@app.cli.command()
+def sayHello():
+    """
+    just say hello
+    """
+    click.echo('hello')
