f4d94b680b8f0724ee8a2818745c6cb24d7c7347
ice <ice45571@163.com>
2018-11-22 20:02:30 +0800

    feat: 登录注册只支持POST请求 表单传输

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, make_response, session
 from jinja2 import escape
 from account import Account
 import os
+import redis
 
 
 def after_request(response):
@@ -16,6 +17,12 @@ app.secret_key = os.getenv('SECRET_KEY')
 app.session_cookie_name = 'encryptCookie'
 app.after_request(after_request)
 accountManager = Account()
+redis = redis.Redis(host='127.0.0.1', port=6379, decode_responses=True)
+
+
+@app.before_first_request
+def beforeFirstRequest():
+    session.permanent = True
 
 
 @app.route('/welcome')
@@ -47,20 +54,38 @@ def testCookie():
     response.set_cookie('account', 'ice')
 
     # 加密
-    session['encryptAccount'] = 'encryptIce'
+    session['encryptAccount'] = 'encryptIce'  # Flask再视图函数结束时会将session中的值都传递到客户端
     # session.pop('encryptAccount')
     # abort(403)
     return response
 
 
-@app.route('/account/register/<string:accountnumber>/<string:password>')
-def register(accountnumber, password):
-    return accountManager.register(accountnumber, password)
+@app.route('/register', methods=['POST'])
+def register():
+    account = request.form['account']
+    password = request.form['password']
+    if redis.hexists('account', account):
+        return jsonify(msg='注册失败,账号已存在!'), '100'
+    else:
+        if redis.hset('account', account, password):
+            session['account'] = account
+            session['password'] = password
+            return jsonify(msg='注册成功!')
+        else:
+            return jsonify(msg='redis存储失败')
 
 
-@app.route('/account/login/<string:accountnumber>/<string:password>')
-def login(accountnumber, password):
-    return accountManager.login(accountnumber, password)
+@app.route('/login', methods=['POST'])
+def login():
+    account = request.form['account']
+    password = request.form['password']
+    accountFromRedis = redis.hget('account', account)
+    if str(password) == str(accountFromRedis):
+        session['account'] = account
+        session['password'] = password
+        return jsonify(msg='登录成功')
+    else:
+        return jsonify(msg='账号密码不匹配')
 
 
 if __name__ == '__main__':
